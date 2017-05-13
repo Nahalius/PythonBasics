e35e262a377b6c62878481b5b9f8cd315c54152c
Nahalius <ivan.v.petkov@gmail.com>
2017-05-13 12:34:34 +0300

    some basics ioj

---
+++ Simple/55triangle.py
@@ -0,0 +1,9 @@
+n = 10
+a = '*'
+
+
+for i in range(0, n):
+    print(i * a )
+
+print (n * a)
+#Prints n-rows triangle with asterics
---
+++ Simple/num1to20.py
@@ -0,0 +1,6 @@
+n = 20
+
+for i in range(1, n+1):
+    print(i)
+
+#Prints the numbers 1 to 20
